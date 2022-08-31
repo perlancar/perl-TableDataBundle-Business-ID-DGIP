c2de79b85779ce1a942431ca727276983f76e49d
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2022-08-31 23:19:47 +0700

    Initial commit

---
+++ lib/TableDataBundle/Business/ID/DGIP.pm
@@ -0,0 +1,26 @@
+# no code
+## no critic: TestingAndDebugging::RequireUseStrict
+package TableDataBundle::Business::ID::DGIP;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Collection of TableData:: modules related to the Directorate General of Intellectual Property
+
+=head1 DESCRIPTION
+
+This distribution contains the following L<TableData>:: modules:
+
+# INSERT_MODULES_LIST /^TableData::/
+
+
+=head1 SEE ALSO
+
+L<TableData>
+
+L<https://dgip.go.id>
+
+=cut
